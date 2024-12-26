b17ae86e444960063bc2fd9e9700b2b6def2585c
tnm <t@ted.io>
2024-12-26 11:42:43 -0800

    Release 0.2.0

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ export { Hypersonic } from './core/pr';
 export { HypersonicConfig, PRConfig, MergeStrategy } from './core/config';
 export { GitHubError } from './core/errors';
 
-export const version = '0.1.0';
+export const version = '0.2.0';
